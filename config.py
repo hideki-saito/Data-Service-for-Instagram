610ac288be73f239f9d48a53e61a4d3d54d5d47f
hideki <saito_hideki.1127@outlook.com>
2017-05-31 23:19:05 +0900

    save settings of the account

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 username = "username"
 password = "password"
 
+settings_file_path = "settings"
+
 host = "localhost"
 port = 27017
--- main.py
+++ main.py
@@ -1,10 +1,43 @@
+import os
 import sys
 import logging
+import codecs
+import json
+import argparse
 from datetime import datetime, timedelta
 from pymongo import MongoClient
 
 from config import *
-from instagram_private_api import Client
+try:
+    from instagram_private_api import (
+        Client, ClientError, ClientLoginError,
+        ClientCookieExpiredError, ClientLoginRequiredError,
+        __version__ as client_version)
+except ImportError:
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    from instagram_private_api import (
+        Client, ClientError, ClientLoginError,
+        ClientCookieExpiredError, ClientLoginRequiredError,
+        __version__ as client_version)
+
+
+def to_json(python_object):
+    if isinstance(python_object, bytes):
+        return {'__class__': 'bytes',
+                '__value__': codecs.encode(python_object, 'base64').decode()}
+    raise TypeError(repr(python_object) + ' is not JSON serializable')
+
+def from_json(json_object):
+    if '__class__' in json_object and json_object['__class__'] == 'bytes':
+        return codecs.decode(json_object['__value__'].encode(), 'base64')
+    return json_object
+
+def onlogin_callback(api, new_settings_file):
+    cache_settings = api.settings
+    with open(new_settings_file, 'w') as outfile:
+        json.dump(cache_settings, outfile, default=to_json)
+        print('SAVED: {0!s}'.format(new_settings_file))
 
 def getAttribute(obj):
     output = {}
@@ -21,9 +54,51 @@ def getAttribute(obj):
 
 class Insgram_DataService():
     def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.client = Client(username, password)
+        device_id = None
+        try:
+            settings_file = settings_file_path
+            if not os.path.isfile(settings_file):
+                # settings file does not exist
+                print('Unable to find file: {0!s}'.format(settings_file_path))
+
+                # login new
+                self.client = Client(
+                    username, password,
+                    on_login=lambda x: onlogin_callback(x, settings_file_path))
+            else:
+                with open(settings_file) as file_data:
+                    cached_settings = json.load(file_data, object_hook=from_json)
+                print('Reusing settings: {0!s}'.format(settings_file))
+
+                device_id = cached_settings.get('device_id')
+                # reuse auth settings
+                self.client = Client(
+                    username, password,
+                    settings=cached_settings)
+
+        except (ClientCookieExpiredError, ClientLoginRequiredError) as e:
+            print('ClientCookieExpiredError/ClientLoginRequiredError: {0!s}'.format(e))
+
+            # Login expired
+            # Do relogin but use default ua, keys and such
+            self.client = Client(
+                username, password,
+                device_id=device_id,
+                on_login=lambda x: onlogin_callback(x, settings_file_path))
+
+        except ClientLoginError as e:
+            print('ClientLoginError {0!s}'.format(e))
+            exit(9)
+        except ClientError as e:
+            print('ClientError {0!s} (Code: {1:d}, Response: {2!s})'.format(e.msg, e.code, e.error_response))
+            exit(9)
+        except Exception as e:
+            print('Unexpected Exception: {0!s}'.format(e))
+            exit(99)
+
+        # self.username = username
+        # self.password = password
+        # self.client = Client(username, password)
         self.user_id = self.client.username_info(username)['user']['pk']
         client = MongoClient(host, port)
         self.db = client.instagram
